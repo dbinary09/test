63075f23c08df3e0cbbda401a5e664fcfdfba0a8
dbinary09 <Desmund.Yeng@MYNB172.fusionex.local>
2015-03-12 19:19:02 +0800

    'a'

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', 'username', msg);
+  socket.on('chat message', function(sendername, msg){
+    io.emit('chat message', sendername, msg);
   });
 });
 
